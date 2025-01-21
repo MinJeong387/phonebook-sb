d0dab7a59fdc92c3b52664e84df980ac253933fc
서민정 <ybsm0629@gmail.com>
2025-01-22 02:57:02 +0900

    commit

---
+++ src/main/java/himedia/phonebook/mappers/PhonebookMapper.java
@@ -0,0 +1,16 @@
+package himedia.phonebook.mappers;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import himedia.phonebook.repository.vo.PhonebookVo;
+
+// 1:1 매칭
+//	이 매퍼 인터페이스를 mybatis mapper 설정의 namespace로 등록해 줘야 한다.
+@Mapper	//	MyBatis Mapper
+public interface PhonebookMapper {
+//	<select id="selectAll" resultType="phonebookVo">
+	List<PhonebookVo> selectAll();
+
+}
